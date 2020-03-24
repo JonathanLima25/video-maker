2cb3777ed42169c5458920361a3ddee85b691dc5
JonathanLima25 <jonathan.lima.sjc23@gmail.com>
2020-03-24 13:16:44 -0300

    adicionando algorithmia

---
+++ robots/text.js
@@ -0,0 +1,17 @@
+const algorithmia = require('algorithmia')
+
+function robot(content) {
+    fetchContentFromWikipedia(content)
+    sanitizeContent(content)
+    breakContentIntoSentences(content)
+
+    function fetchContentFromWikipedia(content) {
+        const algorithmiaAuthenticated = algorithmia('API KEY TEMPORARIA')
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponse = wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponse.get()
+        console.log(wikipediaContent)
+    }
+}
+
+module.exports = robot
